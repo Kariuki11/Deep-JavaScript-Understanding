1bc20da852a2280347e1587dd7638e8ea4dd76d7
Kariuki11 <irungukenneth433@gmail.com>
2025-08-20 18:45:45 +0300

    Node Codding Challenge3

--- Node Js Deep Understanding/Challenges/challenge3.js
+++ Node Js Deep Understanding/Challenges/challenge3.js
@@ -1,3 +1,9 @@
+// You are given an array A consisting of an odd number of integers.
+//     Each element of the array can be paired with another element that has the same value, except for one element that occurs odd number of times (once, three times, etc.).
+//     Write a function in Node.js that finds and returns the value of that unpaired element
+
+
+
 function findUnpaired(A) {
     let result = 0;
     for (let num of A) {
